a8f9ea860a1e025ccf1db0d6c787622f801b02ad
SarangVarmaa <158544997+SarangVarmaa@users.noreply.github.com>
2024-08-22 17:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+
+        display.value = "Error";
+    }
+    
+}
+
+document.addEventListener("keyup",event =>{
+
+        if(event.key.startsWith("Enter")){
+            try{
+                display.value = eval(display.value);
+            }
+            catch(error){
+        
+                display.value = "Error";
+            }
+        }
+})
